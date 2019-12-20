9d6960a93a267178d40b60a33b95175c25321d46
Veronika Dodda <v.shengeliya@gmail.com>
2019-12-20 03:09:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -5,43 +5,43 @@ def welcome
 puts "Welcome to the Blackjack Table"
 end
 
-def deal_card
-  # code #deal_card here
-end
+# def deal_card
+#   # code #deal_card here
+# end
 
-def display_card_total
-  # code #display_card_total here
-end
+# def display_card_total
+#   # code #display_card_total here
+# end
 
-def prompt_user
-  # code #prompt_user here
-end
+# def prompt_user
+#   # code #prompt_user here
+# end
 
-def get_user_input
-  # code #get_user_input here
-end
+# def get_user_input
+#   # code #get_user_input here
+# end
 
-def end_game
-  # code #end_game here
-end
+# def end_game
+#   # code #end_game here
+# end
 
-def initial_round
-  # code #initial_round here
-end
+# def initial_round
+#   # code #initial_round here
+# end
 
-def hit?
-  # code hit? here
-end
+# def hit?
+#   # code hit? here
+# end
 
-def invalid_command
-  # code invalid_command here
-end
+# def invalid_command
+#   # code invalid_command here
+# end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+# #####################################################
+# # get every test to pass before coding runner below #
+# #####################################################
 
-def runner
-  # code runner here
-end
+# def runner
+#   # code runner here
+# end
     
