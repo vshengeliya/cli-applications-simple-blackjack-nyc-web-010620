6dfd9d5739359438e9d3304e3b2233fe07a96286
Veronika Dodda <v.shengeliya@gmail.com>
2019-12-20 04:29:08 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,71 @@
+  # expect($stdout).to receive(:puts).with("Welcome to the Blackjack Table")
+
+
 def welcome
-  # code #welcome here
+puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+    rand(1...11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+ puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game (card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
+
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+def hit?(card_total)
+  valid_inputs = ["h", "s"]
 
-def runner
-  # code runner here
+  prompt_user
+  user_input = get_user_input
+
+  if user_input == "h"
+    card_total += deal_card
+  elsif user_input == "s"
+    card_total
+  else
+    invalid_command
+    hit?(card_total)
+  end
+  
 end
+
+# #####################################################
+# # get every test to pass before coding runner below #
+# #####################################################
+
     
+def runner
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
+end
+   
+   
