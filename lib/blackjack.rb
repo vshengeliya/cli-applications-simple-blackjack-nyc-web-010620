e465e6f47da2a092ba2f7f450f1929151c9b4512
Veronika Dodda <v.shengeliya@gmail.com>
2019-12-20 03:36:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -13,9 +13,9 @@ def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
 end
 
-# def prompt_user
-#   # code #prompt_user here
-# end
+def prompt_user
+  # code #prompt_user here
+end
 
 # def get_user_input
 #   # code #get_user_input here
